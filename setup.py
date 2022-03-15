852c1aacfcd35d9542c395b8f26db8be7d18338e
Tony Bonnaire <bonnaire@TEX.local>
2022-03-15 15:47:49 +0100

    New structure

---
+++ setup.py
@@ -0,0 +1,17 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='trex_python',
+    version='0.1.0',    
+    description='A Python implementation of the T-ReX algorithm',
+    url='https://git.ias.u-psud.fr/tbonnair/t-rex.git',
+    author='Tony Bonnaire',
+    author_email='bonnaire.tonyim@gmail.com',
+    license='GNU General Public License v3.0',
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.7",
+)
